e690f059cdd910435f4a16c96d0a54344d933b37
theoheron <theron@ucsd.edu>
2026-02-26 19:42:25 -0800

    Added two new songs

--- js/tracks.js
+++ js/tracks.js
@@ -2,4 +2,6 @@
 export const TRACKS = [
   { title: "Track 01 — In my zone", file: "songs/track1.mp3" },
   { title: "Track 02 — Pasadena", file: "songs/track2.mp3" },
+  { title: "Track 03 — Travi$ Scott Remix", file: "songs/Travi$ Scott Remix.mp3" },
+  { title: "Track 04 — On my way", file: "songs/On my way.mp3" },
 ];
